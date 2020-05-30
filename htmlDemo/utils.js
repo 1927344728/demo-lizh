24804d6d5e8d73fa73f9127bd4577945b00ea32b
lizhao <1927344728@qq.com>
2020-05-30 18:58:34 +0800

    视频video Demo 5.30

---
+++ htmlDemo/utils.js
@@ -0,0 +1,11 @@
+let Utils = {
+	getUrlParam (name) {
+		let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
+		let r = window.location.search.substr(1).match(reg)
+		if (r != null) {
+			return decodeURIComponent(r[2])
+		}
+		return null
+	}
+}
+window.Utils = Utils
