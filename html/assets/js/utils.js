58e31156f68c7654a3d8234011d935b30e779808
lizhao <1927344728@qq.com>
2021-10-11 22:42:21 +0800

    项目结构整理 10.11

---
+++ html/assets/js/utils.js
@@ -0,0 +1,33 @@
+// https://my-files-1259410276.cos.ap-chengdu.myqcloud.com/md/images/js/image-20200803225915556.png
+let Utils = {
+	getUrlParam: name => {
+		let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
+		let r = window.location.search.substr(1).match(reg)
+		if (r != null) {
+			return decodeURIComponent(r[2])
+		}
+		return null
+	},
+	ready: () => {
+    return new Promise((resolve, reject) => {
+      let type = Utils.getUrlParam('type') * 1 || 1
+      if (
+        document.readyState === "complete" ||
+        (document.readyState !== "loading" && !document.documentElement.doScroll)
+      ) {
+        Array.prototype.slice.apply(document.querySelectorAll('section')).map((ele, i) => {
+          ele.style.display = i === type - 1 ? 'block' : 'none'
+        })
+        resolve()
+      } else {
+        document.addEventListener("DOMContentLoaded", () => {
+          Array.prototype.slice.apply(document.querySelectorAll('section')).map((ele, i) => {
+            ele.style.display = i === type - 1 ? 'block' : 'none'
+          })
+          resolve()          
+        })
+      } 
+    })
+	}
+}
+window.Utils = Utils
